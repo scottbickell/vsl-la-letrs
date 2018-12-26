a050fb3fd9fc387eff2ff7ce061fbb1a613b3df8
Scott Bickell <bickellworks@gmail.com>
2018-12-26 16:17:37 -0700

    Add alternative layout with new content tab for testimonials

--- js/custom.js
+++ js/custom.js
@@ -1,10 +1,10 @@
-$('#link, #thumbnail').click(function () {
-    var src = 'https://www.youtube.com/embed/3wd95mueUQo';
-    $('#myModal').modal('show');
-    $('#myModal iframe').attr('src', src);
-});
+// $('#link, #thumbnail').click(function () {
+//     var src = 'https://www.youtube.com/embed/3wd95mueUQo';
+//     $('#myModal').modal('show');
+//     $('#myModal iframe').attr('src', src);
+// });
 
-$('#myModal button').click(function () {
-    $('#myModal iframe').removeAttr('src');
-});
+// $('#myModal button').click(function () {
+//     $('#myModal iframe').removeAttr('src');
+// });
 
